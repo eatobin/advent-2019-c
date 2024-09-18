e01e1c3ee61315a314bee366a9fa4226135cf07a
Eric Tobin <eatobin@gmail.com>
2024-09-18 14:54:19 -0700

    great education -2

--- day03/main.c
+++ day03/main.c
@@ -16,14 +16,41 @@
 //    return 0;
 //}
 
+//#include <malloc.h>
+//
+//int *myFunction() {
+//    int *ptr;
+//    ptr = malloc(sizeof(*ptr));
+//    *ptr = 67;
+//    printf("Hi! %d\n", *ptr);
+//    printf("Hi again! %p\n", ptr);
+//    return ptr;
+//}
+//
+//int main() {
+//    int *ptrAddress;
+//    ptrAddress = myFunction();
+//    printf("Hi #3! %d\n", *ptrAddress);
+//    printf("Hi again#4! %p\n", ptrAddress);
+//    *ptrAddress = *ptrAddress * 20;
+//    printf("Last one! %d\n", *ptrAddress);
+//    printf("The function has ended\n");
+//    free(ptrAddress);
+//    ptrAddress = NULL;
+//    return 0;
+//}
+
 #include <malloc.h>
 
 int *myFunction() {
     int *ptr;
-    ptr = malloc(sizeof(*ptr));
+    ptr = malloc(sizeof(*ptr) * 3);
     *ptr = 67;
+    *(ptr + 1) = 68;
     printf("Hi! %d\n", *ptr);
     printf("Hi again! %p\n", ptr);
+    printf("HiXX! %d\n", *(ptr + 1));
+    printf("Hi againP! %p\n", ptr + 1);
     return ptr;
 }
 
@@ -34,6 +61,7 @@ int main() {
     printf("Hi again#4! %p\n", ptrAddress);
     *ptrAddress = *ptrAddress * 20;
     printf("Last one! %d\n", *ptrAddress);
+    printf("Last one-not really! %d\n", *(ptrAddress + 1));
     printf("The function has ended\n");
     free(ptrAddress);
     ptrAddress = NULL;
