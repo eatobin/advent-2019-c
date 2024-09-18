d5a2eb5a93cdaf42fcf51631fc6672896648adcc
Eric Tobin <eatobin@gmail.com>
2024-09-18 14:39:41 -0700

    great education

--- day03/main.c
+++ day03/main.c
@@ -1,17 +1,41 @@
+//#include <malloc.h>
+//
+//void myFunction() {
+//    int *ptr;
+//    ptr = malloc(sizeof(*ptr));
+//    *ptr = 67;
+//    printf("Hi! %d\n", *ptr);
+//    printf("Hi again! %p\n", ptr);
+//    free(ptr);
+//    ptr = NULL;
+//}
+//
+//int main() {
+//    myFunction();
+//    printf("The function has ended");
+//    return 0;
+//}
+
 #include <malloc.h>
 
-void myFunction() {
+int *myFunction() {
     int *ptr;
     ptr = malloc(sizeof(*ptr));
     *ptr = 67;
     printf("Hi! %d\n", *ptr);
     printf("Hi again! %p\n", ptr);
-    free(ptr);
-    ptr = NULL;
+    return ptr;
 }
 
 int main() {
-    myFunction();
-    printf("The function has ended");
+    int *ptrAddress;
+    ptrAddress = myFunction();
+    printf("Hi #3! %d\n", *ptrAddress);
+    printf("Hi again#4! %p\n", ptrAddress);
+    *ptrAddress = *ptrAddress * 20;
+    printf("Last one! %d\n", *ptrAddress);
+    printf("The function has ended\n");
+    free(ptrAddress);
+    ptrAddress = NULL;
     return 0;
 }
