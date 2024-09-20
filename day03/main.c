e61d3eeb1ac7fc1b3c331ba8f35358aa2a9eb386
Eric Tobin <eatobin@gmail.com>
2024-09-19 17:34:36 -0700

    struct progress

--- day03/main.c
+++ day03/main.c
@@ -95,41 +95,88 @@
 //    return 0;
 //}
 
+//#include <malloc.h>
+//
+//char *myFunction() {
+//    char *ptr;
+//    ptr = malloc(sizeof(char) * 4);
+//    *(ptr + 0) = 78;
+//    *(ptr + 1) = 111;
+//    *(ptr + 2) = 0;
+//    *(ptr + 3) = 0;
+//    return ptr;
+//}
+//
+//int main() {
+//    char *ptrAddress;
+//    ptrAddress = myFunction();
+//    for (char i = 0; i < 4; ++i) {
+//        printf("Contents: %c\n", *(ptrAddress + i));
+//        printf("Address: %p\n", ptrAddress + i);
+//    }
+//    puts(ptrAddress);
+//    printf("Again: %s\n", ptrAddress);
+//    *(ptrAddress + 0) = 89;
+//    *(ptrAddress + 1) = 101;
+//    *(ptrAddress + 2) = 115;
+//    for (char i = 0; i < 4; ++i) {
+//        printf("Contents: %c\n", *(ptrAddress + i));
+//        printf("Address: %p\n", ptrAddress + i);
+//    }
+//    puts(ptrAddress);
+//    printf("Again: %s\n", ptrAddress);
+//    for (char i = 0; i < 4; ++i) {
+//        printf("Contents: %d\n", *(ptrAddress + i));
+//        printf("Address: %p\n", ptrAddress + i);
+//    }
+//    free(ptrAddress);
+//    ptrAddress = NULL;
+//    return 0;
+//}
+
+//#include <malloc.h>
+//
+//double *myFunction() {
+//    double *ptr;
+//    ptr = malloc(sizeof(double) * 3);
+//    *(ptr + 0) = 60.0;
+//    *(ptr + 1) = 61.1;
+//    *(ptr + 2) = 62.2;
+//    return ptr;
+//}
+//
+//int main() {
+//    double *ptrAddress;
+//    ptrAddress = myFunction();
+//    for (char i = 0; i < 3; ++i) {
+//        printf("Contents: %lf\n", *(ptrAddress + i));
+//        printf("Address: %p\n", ptrAddress + i);
+//    }
+//    *(ptrAddress + 0) = *(ptrAddress + 0) * 2;
+//    *(ptrAddress + 2) = *(ptrAddress + 2) * 2;
+//    printf("0 times 2: %lf\n", *(ptrAddress + 0));
+//    printf("2 times 2: %lf\n", *(ptrAddress + 2));
+//    free(ptrAddress);
+//    ptrAddress = NULL;
+//    return 0;
+//}
+
 #include <malloc.h>
 
-char *myFunction() {
-    char *ptr;
-    ptr = malloc(sizeof(char) * 4);
-    *(ptr + 0) = 78;
-    *(ptr + 1) = 111;
-    *(ptr + 2) = 0;
-    *(ptr + 3) = 0;
-    return ptr;
+struct MyStructure {
+    int myNum;
+};
+
+struct MyStructure *myFunction() {
+    struct MyStructure s1;
+    s1.myNum = 13;
+    return &s1;
 }
 
 int main() {
-    char *ptrAddress;
+    struct MyStructure *ptrAddress;
     ptrAddress = myFunction();
-    for (char i = 0; i < 4; ++i) {
-        printf("Contents: %c\n", *(ptrAddress + i));
-        printf("Address: %p\n", ptrAddress + i);
-    }
-    puts(ptrAddress);
-    printf("Again: %s\n", ptrAddress);
-    *(ptrAddress + 0) = 89;
-    *(ptrAddress + 1) = 101;
-    *(ptrAddress + 2) = 115;
-    for (char i = 0; i < 4; ++i) {
-        printf("Contents: %c\n", *(ptrAddress + i));
-        printf("Address: %p\n", ptrAddress + i);
-    }
-    puts(ptrAddress);
-    printf("Again: %s\n", ptrAddress);
-    for (char i = 0; i < 4; ++i) {
-        printf("Contents: %d\n", *(ptrAddress + i));
-        printf("Address: %p\n", ptrAddress + i);
-    }
-    free(ptrAddress);
-    ptrAddress = NULL;
+    printf("Contents: %d\n", ptrAddress->myNum);
+    printf("Address: %p\n", ptrAddress);
     return 0;
 }
