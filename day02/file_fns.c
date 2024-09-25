19f59ca535def6cb880da54b2f333a38bf12d7ad
Eric Tobin <eatobin@gmail.com>
2024-09-25 16:31:39 -0700

    better free

--- day02/file_fns.c
+++ day02/file_fns.c
@@ -68,7 +68,6 @@ int *make_memory(int *memory, char *string) {
         token_at_index = strtok(NULL, ",");
         index++;
     }
-    free(string);
     return memory;
 }
 
@@ -83,6 +82,7 @@ struct Memory return_memory(char *file_path) {
     initialized_int_array = create_initialized_int_array(memory_length);
     memory.contents = make_memory(initialized_int_array, string);
     memory.length = memory_length;
+    free(string);
     string = NULL;
 
     return memory;
