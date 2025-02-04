b14ce062370b981692a5782f5df5b5cc4c896207
Eric Tobin <eatobin@gmail.com>
2025-02-03 17:25:33 -0700

    progress

--- day05/day05.c
+++ day05/day05.c
@@ -3,83 +3,50 @@
 
 typedef struct Intcode {
     int pointer;
-    int memory[121];
+    int memory[678];
 } anIntcode;
 
 anIntcode makeIntcode(void);
-int opcode(anIntcode *intcode);
-void updated_memory(anIntcode *intcode, int noun, int verb);
-int noun_verb(void);
+// int opcode(anIntcode *intcode);
 
 int main(void) {
     anIntcode intcode = makeIntcode();
 
-    int ic_return = 1;
-    updated_memory(&intcode, 12, 2);
-
-    while (ic_return == 1) {
-        ic_return = opcode(&intcode);
-    }
-
     printf("Part A answer = %d. Correct = 2890696\n", intcode.memory[0]);
-    printf("Part B answer = %d. Correct = 8226\n", noun_verb());
+    printf("Part A answer = %d. Correct = 2890696\n", intcode.memory[676]);
+    // printf("Part B answer = %d. Correct = 8226\n", noun_verb());
 
     return 0;
 }
 
 anIntcode makeIntcode(void) {
-    const int memoryContents[121] = {1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 9, 19, 23, 2, 13, 23, 27, 1, 6, 27, 31, 2, 6, 31, 35, 2, 13, 35, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 9, 47, 51, 1, 51, 13, 55, 1, 55, 13, 59, 2, 59, 13, 63, 1, 63, 6, 67, 2, 6, 67, 71, 1, 5, 71, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 6, 87, 1, 5, 87, 91, 1, 6, 91, 95, 2, 95, 6, 99, 1, 5, 99, 103, 1, 6, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0, 0};
+    const int memoryContents[678] = {3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 79, 14, 225, 1101, 17, 42, 225, 2, 74, 69, 224, 1001, 224, -5733, 224, 4, 224, 1002, 223, 8, 223, 101, 4, 224, 224, 1, 223, 224, 223, 1002, 191, 83, 224, 1001, 224, -2407, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 223, 224, 223, 1101, 18, 64, 225, 1102, 63, 22, 225, 1101, 31, 91, 225, 1001, 65, 26, 224, 101, -44, 224, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 224, 223, 223, 101, 78, 13, 224, 101, -157, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 3, 224, 1, 224, 223, 223, 102, 87, 187, 224, 101, -4698, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 4, 224, 1, 223, 224, 223, 1102, 79, 85, 224, 101, -6715, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 2, 224, 1, 224, 223, 223, 1101, 43, 46, 224, 101, -89, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 1, 224, 224, 1, 223, 224, 223, 1101, 54, 12, 225, 1102, 29, 54, 225, 1, 17, 217, 224, 101, -37, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 223, 224, 223, 1102, 20, 53, 225, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 107, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 329, 101, 1, 223, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 344, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 359, 101, 1, 223, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 374, 101, 1, 223, 223, 8, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 389, 101, 1, 223, 223, 1108, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 404, 101, 1, 223, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 419, 101, 1, 223, 223, 8, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 434, 1001, 223, 1, 223, 1008, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 449, 1001, 223, 1, 223, 1008, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 464, 101, 1, 223, 223, 1107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 479, 101, 1, 223, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 494, 1001, 223, 1, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 509, 101, 1, 223, 223, 1108, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 524, 101, 1, 223, 223, 7, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 539, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 554, 101, 1, 223, 223, 8, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 569, 1001, 223, 1, 223, 1008, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 584, 101, 1, 223, 223, 107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 599, 1001, 223, 1, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 614, 101, 1, 223, 223, 1007, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 629, 101, 1, 223, 223, 1107, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 644, 101, 1, 223, 223, 108, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 659, 101, 1, 223, 223, 1007, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 674, 101, 1, 223, 223, 4, 223, 99, 226};
     anIntcode intcode;
     intcode.pointer = 0;
     memcpy(intcode.memory, memoryContents, sizeof(memoryContents));
     return intcode;
 }
 
-int opcode(anIntcode *intcode) {
-    const int action = intcode->memory[intcode->pointer];
-    const int address1 = intcode->memory[intcode->pointer + 1];
-    const int address2 = intcode->memory[intcode->pointer + 2];
-    const int address3 = intcode->memory[intcode->pointer + 3];
-
-    switch (action) {
-        case 1:
-            intcode->memory[address3] =
-                    intcode->memory[address1] +
-                    intcode->memory[address2];
-            intcode->pointer += 4;
-            return 1;
-        case 2:
-            intcode->memory[address3] =
-                    intcode->memory[address1] *
-                    intcode->memory[address2];
-            intcode->pointer += 4;
-            return 1;
-        default:
-            return 0;
-    }
-}
-
-void updated_memory(anIntcode *intcode, const int noun, const int verb) {
-    intcode->memory[1] = noun;
-    intcode->memory[2] = verb;
-}
-
-int noun_verb(void) {
-    for (int noun = 0; noun < 100; noun++) {
-        for (int verb = 0; verb < 100; verb++) {
-            anIntcode intcode = makeIntcode();
-            updated_memory(&intcode, noun, verb);
-
-            int ic_return = 1;
-            while (ic_return == 1) {
-                ic_return = opcode(&intcode);
-            }
-
-            const int candidate = intcode.memory[0];
-            if (candidate == 19690720) {
-                return 100 * noun + verb;
-            }
-        }
-    }
-    return -1;
-}
+// int opcode(anIntcode *intcode) {
+//     const int action = intcode->memory[intcode->pointer];
+//     const int address1 = intcode->memory[intcode->pointer + 1];
+//     const int address2 = intcode->memory[intcode->pointer + 2];
+//     const int address3 = intcode->memory[intcode->pointer + 3];
+//
+//     switch (action) {
+//         case 1:
+//             intcode->memory[address3] =
+//                     intcode->memory[address1] +
+//                     intcode->memory[address2];
+//             intcode->pointer += 4;
+//             return 1;
+//         case 2:
+//             intcode->memory[address3] =
+//                     intcode->memory[address1] *
+//                     intcode->memory[address2];
+//             intcode->pointer += 4;
+//             return 1;
+//         default:
+//             return 0;
+//     }
+// }
