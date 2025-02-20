793449c7d406a34635b55cad88b8c2e071b4d011
Eric Tobin <eatobin@gmail.com>
2025-02-19 21:54:34 -0700

    my own ideas for a function!

--- day05/day05.c
+++ day05/day05.c
@@ -2,11 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *createArray(int size);
+int *doubleArray(int *array, int size);
+
+int main() {
+    // Define the size of the array
+    const int size = 5;
+    // Call the function to create the array and store the
+    // returned pointer
+    int *myArray = createArray(size);
+
+    // Print the elements of the array
+    printf("Array Elements: ");
+    for (int i = 0; i < size; i++) {
+
+        printf("%d ", myArray[i]);
+    }
+    printf("\n");
+
+    myArray[0] = 12;
+
+    printf("Array Elements: ");
+    for (int i = 0; i < size; i++) {
+
+        printf("%d ", myArray[i]);
+    }
+    printf("\n");
+
+    myArray = doubleArray(myArray, size);
+
+    printf("Array Elements: ");
+    for (int i = 0; i < size; i++) {
+
+        printf("%d ", myArray[i]);
+    }
+    printf("\n");
+
+    // Free the allocated memory to avoid memory leaks
+    free(myArray);
+
+    return 0;
+}
+
 // Function to create and return an array using dynamic
 // memory allocation
-int *createArray(int size) {
+int *createArray(const int size) {
     // Allocate memory for the array
-    int *array = (int *) malloc(size * sizeof(int));
+    int *array = malloc(size * sizeof(int));
 
     // Check if memory allocation was successful
     if (array == NULL) {
@@ -23,23 +65,9 @@ int *createArray(int size) {
     return array;
 }
 
-int main() {
-    // Define the size of the array
-    int size = 5;
-    // Call the function to create the array and store the
-    // returned pointer
-    int *myArray = createArray(size);
-
-    // Print the elements of the array
-    printf("Array Elements: ");
+int *doubleArray(int *array, const int size) {
     for (int i = 0; i < size; i++) {
-
-        printf("%d ", myArray[i]);
+        array[i] = array[i] * 2;
     }
-    printf("\n");
-
-    // Free the allocated memory to avoid memory leaks
-    free(myArray);
-
-    return 0;
+    return array;
 }
