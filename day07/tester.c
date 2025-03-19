adc5781ec3966b798bc02969652fd699e42e5cc4
Eric Tobin <eatobin@gmail.com>
2025-03-18 20:44:20 -0700

    typo

--- day07/tester.c
+++ day07/tester.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void swap(int *x, int *y) {
-    int temp = *x;
+    const int temp = *x;
     *x = *y;
     *y = temp;
 }
 
-void pemutations(int k, int A[]) {
+void permutations(const int k, int A[]) {
     if (k == 1) {
         printf("Array element: ");
         for (int i = 0; i < 3; i++) {
@@ -15,20 +15,20 @@ void pemutations(int k, int A[]) {
         printf("\n");
     } else {
         for (int i = 0; i < k - 1; ++i) {
-            pemutations(k - 1, A);
+            permutations(k - 1, A);
             if (k % 2 == 0) {
                 swap(&A[i], &A[k - 1]);
             } else {
                 swap(&A[0], &A[k - 1]);
             }
         }
-        pemutations(k - 1, A);
+        permutations(k - 1, A);
     }
 }
 
 int main() {
     int A[] = {1, 2, 3};
-    int len = sizeof(A) / sizeof(A[0]);
-    pemutations(len, A);
+    const int len = sizeof(A) / sizeof(A[0]);
+    permutations(len, A);
     return 0;
 }
